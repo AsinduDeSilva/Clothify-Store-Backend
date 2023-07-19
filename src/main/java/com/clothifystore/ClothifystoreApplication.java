650b49c856e35f894aa7569c29b647332b72342f
Asindu De Silva <asindudesilva@gmail.com>
2023-07-19 16:54:10 +0530

    initial commit

---
+++ src/main/java/com/clothifystore/ClothifystoreApplication.java
@@ -0,0 +1,13 @@
+package com.clothifystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothifystoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothifystoreApplication.class, args);
+	}
+
+}
