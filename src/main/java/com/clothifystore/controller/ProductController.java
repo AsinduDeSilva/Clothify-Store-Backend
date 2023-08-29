e835a98ae0a29028ba50d3277f5e7ac92c91ed85
Asindu De Silva <asindudesilva@gmail.com>
2023-08-30 01:04:25 +0530

    Implemented necessary changes for deployment

--- src/main/java/com/clothifystore/controller/ProductController.java
+++ src/main/java/com/clothifystore/controller/ProductController.java
@@ -19,7 +19,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
--- src/main/java/com/clothifystore/entity/Order.java
+++ src/main/java/com/clothifystore/entity/Order.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+
 import java.util.List;
 
 @Entity
